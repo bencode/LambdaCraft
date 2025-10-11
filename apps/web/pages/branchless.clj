dd57c47fa51aa762dc127e3ad54661bc0830bafb
Qijun <qijun.wei@outlook.com>
2025-10-12 00:10:26 +0800

    refactor: remove branchless and chat-with-gpt pages
    - Remove branchless article and related markdown file
    - Remove chat-with-gpt section
    - Remove Practice section from homepage

--- apps/web/pages/branchless.clj
+++
@@ -1,7 +0,0 @@
-^{:nextjournal.clerk/visibility {:code :hide}}
-(ns pages.branchless
-  (:require [nextjournal.clerk :as clerk]
-            [clojure.java.io :as io]))
-
-^{:nextjournal.clerk/visibility {:code :hide}}
-(clerk/md (slurp (io/file "pages/md/branchless.md")))
