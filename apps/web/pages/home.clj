5354b9b06e148db2a833f57f3137f74951ab2066
Qijun <qijun.wei@outlook.com>
2025-10-26 12:25:24 +0800

    refactor: 精简首页项目描述
    简化项目描述，使其更加简洁明了

--- apps/web/pages/home.clj
+++ apps/web/pages/home.clj
@@ -13,7 +13,7 @@
   [:p {:class "bcd-slogan"} "() => study(math) => practice(code)"]])
 
 ;; ## Study
-;; - [每周阅读](https://in.qijun.io)
+;; - [编程技艺周刊](https://in.qijun.io)
 ;; - [我的知识库](/docs/home)
 ;;
 ;; ## Math
@@ -24,8 +24,8 @@
 ;; - [FP](/pages/fp)
 ;;
 ;; ## Code
-;; - [code](https://github.com/bencode/code) - 我的代码练习与算法学习仓库
-;; - [chalkpad](https://github.com/bencode/chalkpad) - 现代化的在线代码编辑器
-;; - [logseq-runit-plugin](https://github.com/bencode/logseq-runit-plugin) - 在 Logseq 笔记中直接运行代码片段，支持 JavaScript、Python、Scheme 和 Clojure
+;; - [code](https://github.com/bencode/code) - 代码实验室
+;; - [chalkpad](https://github.com/bencode/chalkpad) - 代码实验室+
+;; - [logseq-runit-plugin](https://github.com/bencode/logseq-runit-plugin) - 在 Logseq 笔记中运行代码片段
 ;; - [increa.el](https://github.com/bencode/increa.el) - Emacs 智能代码补全插件，提供类似 GitHub Copilot 的幽灵文本补全
-;; - [increa-reader](https://github.com/bencode/increa-reader) - 增强的代码阅读工具
+;; - [increa-reader](https://github.com/bencode/increa-reader) - AI原生阅读器
