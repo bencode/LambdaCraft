596f1482f6e86c12b200684e47e962a5304e0b24
Qijun <qijun.wei@outlook.com>
2025-10-27 23:42:17 +0800

    feat: 在首页 Code 小节添加 codescope 项目

--- apps/web/pages/home.clj
+++ apps/web/pages/home.clj
@@ -26,6 +26,7 @@
 ;; ## Code
 ;; - [code](https://github.com/bencode/code) - 代码实验室
 ;; - [chalkpad](https://github.com/bencode/chalkpad) - 代码实验室+
+;; - [codescope](https://github.com/bencode/codescope) - 代码结构分析工具，用于理解复杂度和依赖关系
 ;; - [logseq-runit-plugin](https://github.com/bencode/logseq-runit-plugin) - 在 Logseq 笔记中运行代码片段
 ;; - [increa.el](https://github.com/bencode/increa.el) - Emacs 智能代码补全插件，提供类似 GitHub Copilot 的幽灵文本补全
 ;; - [increa-reader](https://github.com/bencode/increa-reader) - AI原生阅读器
