2da2e8318dc43b347f43fc69c2ea2494e12b0e5e
Qijun <qijun.wei@outlook.com>
2025-12-30 11:25:37 +0800

    feat: 添加伽罗瓦理论附录（理论框架）

---
+++ apps/web/books/galois/appendix.clj
@@ -0,0 +1,26 @@
+^{:nextjournal.clerk/visibility {:code :hide}}
+(ns books.galois.appendix
+  {:nextjournal.clerk/toc false}
+  (:require [nextjournal.clerk :as clerk]
+            [clojure.java.io :as io]))
+
+^{:nextjournal.clerk/visibility {:code :hide}}
+(clerk/html [:link {:rel "stylesheet" :href "/css/style.css"}])
+
+^{:nextjournal.clerk/visibility {:code :hide}}
+(defn read-md [filename]
+  (let [project-root (System/getProperty "user.dir")
+        parent-dir (-> project-root io/file .getParentFile .getParentFile)
+        md-path (io/file parent-dir "ibooks" "galois" filename)]
+    (slurp md-path)))
+
+;; [← 返回目录](/pages/galois)
+
+;; ---
+
+^{:nextjournal.clerk/visibility {:code :hide :result :show}}
+(clerk/md (read-md "appendix.md"))
+
+;; ---
+
+;; [← 返回目录](/pages/galois)
